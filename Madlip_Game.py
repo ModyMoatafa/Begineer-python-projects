0c62297e49900a648e81c4db06d6d7344382aa55
ModyMoatafa <103070066+ModyMoatafa@users.noreply.github.com>
2023-11-29 10:01:10 +0200

    Add files via upload

---
+++ Madlip_Game.py
@@ -0,0 +1,10 @@
+color=input("Enter a color:\n")
+PluralNoun=input("Enter a Plural noun\n")
+celebrty=input("Enter a celebrty name \n")
+
+
+print("-------------------------------------------------")
+
+print(f"roses are {color}")
+print(f"{PluralNoun} are blue")
+print(f"i love {celebrty}")
---
+++ Translator.py
@@ -0,0 +1,10 @@
+def Translate(phrase):
+    translation=""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation = translation +"g"
+        else:
+            translation=translation +letter
+    return translation
+
+print(Translate(input("Enter a phrase")))
